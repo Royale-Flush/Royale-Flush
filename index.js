058258b170bce14b0a141796b3f24da5607a8dd8
pawanbenjamin <62716484+pawanbenjamin@users.noreply.github.com>
2022-04-21 15:13:02 -0400

    cr1

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-const express = require("express");
-const server = express();
-const cors = require("cors");
-server.use(cors());
+const client = require('./db/client')
+const express = require('express')
+const server = express()
+const cors = require('cors')
+const morgan = require('morgan')
+const path = require('path')
+const PORT = process.env.PORT || 4000
+
+server.use(cors())
 // create logs for everything
-const morgan = require("morgan");
-server.use(morgan("dev"));
-server.use(express.json());
-const path = require("path");
-server.use(express.static(path.join(__dirname, "build")));
-server.use("/api", require("./api"));
+server.use(morgan('dev'))
+
+server.use(express.json())
+
+server.use(express.static(path.join(__dirname, 'build')))
 
 server.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-  next();
-});
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  next()
+})
 
-const client = require("./db/client");
-client.connect();
-const PORT = process.env.PORT || 4000;
+server.use('/api', require('./api'))
 
 // define a server handle to close open tcp connection after unit tests have run
 const handle = server.listen(PORT, async () => {
-  console.log(`Server is running on ${PORT}!`);
+  console.log(`Server is running on ${PORT}!`)
 
   try {
-    client.connect();
-    console.log("Database is open for business!");
+    client.connect()
+    console.log('Database is open for business!')
   } catch (error) {
-    console.error("Database is closed for repairs!\n", error);
+    console.error('Database is closed for repairs!\n', error)
   }
-});
+})
 
-module.exports = { server, handle };
+module.exports = { server, handle }
