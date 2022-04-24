645d3e14789ee08f40a6906826363f64f0882cdc
Alejandro <alexsagarnaga@gmail.com>
2022-04-24 14:49:03 -0500

    created route for new user

--- api/customer.js
+++ api/customer.js
@@ -12,3 +12,23 @@ userRouter.get("/", async (req, res, next) => {
     next({ name: "Definitely an Error", message: "Definitely made a mistake" });
   }
 });
+userRouter.post("/new_user", async (req, res, next) => {
+  const { username, password, name, address, email, phone, payment } = req.body;
+  try {
+    const newUser = await Customer.createUser({
+      username,
+      password,
+      name,
+      address,
+      email,
+      phone,
+      payment,
+    });
+    res.send({ newUser });
+  } catch ({ name, message }) {
+    next({
+      name: "Error",
+      message: "Didn't make a new user",
+    });
+  }
+});
