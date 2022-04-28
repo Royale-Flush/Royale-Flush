a0a6b2c223a81dadc4d2f5833e97df71902627cd
coryla <cory.lance.anderson@gmail.com>
2022-04-28 13:56:16 -0500

    testing create order

--- api/order.js
+++ api/order.js
@@ -1,4 +1,8 @@
-const { editOrders, getOrdersByCustomerId, deleteOrders } = require("../db/models/orders");
+const {
+  editOrders,
+  getOrdersByCustomerId,
+  deleteOrders,
+} = require("../db/models/orders");
 const orderRouter = require("express").Router();
 const { Order } = require("./models");
 const { auth } = require("./utils");
@@ -19,57 +23,52 @@ orderRouter.get("/", async (req, res, next) => {
   }
 });
 
-
 // check route path below...
-orderRouter.get("/:customerId/order", async (req, res, next ) => {
-  const { customerId} = req.params; 
+orderRouter.get("/:customerId/order", async (req, res, next) => {
+  const { customerId } = req.params;
   try {
-    const order = await Order.getOrdersByCustomerId({customerId});
-    res.send({order}); 
-  } catch ({name, message}) {
-    next ({name: "Error" , message: "Error retrieving your order"})
+    const order = await Order.getOrdersByCustomerId({ customerId });
+    res.send({ order });
+  } catch ({ name, message }) {
+    next({ name: "Error", message: "Error retrieving your order" });
   }
-})
+});
 
-orderRouter.post("/", auth, async, (req, res, next) => {
+orderRouter.post("/", async,  (req, res, next) => {
   const { customerId, totalAmount } = req.body;
   try {
-    const newOrder = await Order.createOrders({customerId, totalAmount}); 
-    res.send({newOrder}); 
-  } catch ({name, message}) {
-    next({name: "Error", message: "Error creating order"})
+    const newOrder = await Order.createOrders({ customerId, totalAmount });
+    res.send({ newOrder });
+  } catch ({ name, message }) {
+    next({ name: "Error", message: "Error creating order" });
   }
 });
 
 orderRouter.patch("/:orderId", auth, async (req, res, next) => {
-const { orderId} = req.params; 
-const { totalAmount, isActive} = req.body; 
+  const { orderId } = req.params;
+  const { totalAmount, isActive } = req.body;
 
-try {
-  const editedOrder = await editOrders({
-    id: orderId, 
-    totalAmount, 
-    isActive, 
-  })
-  res.send(editedOrder); 
-} catch ({name, message}) {
-  next({name: "Error", message: "Error editing order"})
-  
-}
-
-} )
-
+  try {
+    const editedOrder = await editOrders({
+      id: orderId,
+      totalAmount,
+      isActive,
+    });
+    res.send(editedOrder);
+  } catch ({ name, message }) {
+    next({ name: "Error", message: "Error editing order" });
+  }
+});
 
 orderRouter.delete("/:orderId", auth, async (req, res, next) => {
-  const id = req.params.orderId; 
+  const id = req.params.orderId;
   try {
-    await getOrdersByCustomerId(id); 
-    const deletedProduct = await deleteOrders({id});
-    res.send(deletedProduct)
-  } catch ({name, message}) {
-    next({name: "Error", message: "Error deleting order"})
+    await getOrdersByCustomerId(id);
+    const deletedProduct = await deleteOrders({ id });
+    res.send(deletedProduct);
+  } catch ({ name, message }) {
+    next({ name: "Error", message: "Error deleting order" });
   }
-})
-
+});
 
 module.exports = orderRouter;
