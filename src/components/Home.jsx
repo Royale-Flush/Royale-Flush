cbec027e326a8088237512403b84b3679fac7bf6
Alejandro <alexsagarnaga@gmail.com>
2022-05-02 12:45:32 -0500

    css formatted onto home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,6 +8,17 @@ const Home = () => {
     <div className="images">
       <img src="https://i.imgur.com/yvCTyaS.png" />
       <h2 id="greetings">Hello, {user.username}</h2>
+      <div>
+        {" "}
+        <img
+          className="toiletCompare"
+          src="http://www.byhyu.com/uploads/4/7/3/7/47371935/difference-between-wall-hung-vs-standard-toilet_orig.png"
+        />{" "}
+        <img
+          className="portaToiletCompare"
+          src="http://www.portablerestroomsusa.com/images/portable-restrooms.jpg"
+        />{" "}
+      </div>
     </div>
   );
 };
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
   const [password, setPassword] = useState("");
   return (
     <div>
-      <h2>Hello</h2>
+      <h2 className="welcome">Please log in and Happy Flushing!</h2>
       <div className="inputBoxes">
         <form
           onSubmit={async (e) => {
